c0cb71ea5801c9cc81f56bc89a218a0f96eff478
yzr <yzr.ouy@gmail.com>
2024-05-21 23:30:49 +0800

    del all

--- src/store/index.js
+++
@@ -1,9 +0,0 @@
-// 组合redux子模块 + 导出store实例
-import { configureStore } from "@reduxjs/toolkit";
-import useReducer from "./modules/user";
-
-export default configureStore({
-    reducer: {
-        user: useReducer 
-    }
-})
